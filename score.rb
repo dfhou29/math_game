6f3b7af29884bffed0a95b811f621c7d7a1016f4
Difan <dfhou29@gmail.com>
2023-09-19 02:52:59 -0700

    add message

--- score.rb
+++ score.rb
@@ -10,8 +10,9 @@ class Score
   def print_score
     if player1.lives == 0
       puts "#{player2.name} wins with a score of #{player2.lives}/3"
+      puts "Goodbye!"
     elsif player2.lives == 0
-      puts "#{player2.name} wins with a score of #{player2.lives}/3"
+      puts "#{player1.name} wins with a score of #{player1.lives}/3"
     else
       puts "#{player1.name}: #{player1.lives}/3 vs #{player2.name}: #{player2.lives}/3"
     end
